d08726e173b3ed1cc87862459158cfa21f1bdabd
David Stanley <dstanley@uoguelph.ca>
2018-09-22 15:35:07 -0400

    Add user specified number of decimals

--- R/apa_descriptive_statistics.R
+++ R/apa_descriptive_statistics.R
@@ -1,9 +1,12 @@
 #' Report descriptive statistics for a set of values
+#' @param ... one or more dplyr commands (separated by commas) to select rows. Eg: alcohol=="2 Pints", gender=="Female"
 #' @param dv Name of the dependent variable column
 #' @param data Project data frame name
-#' @param show.conf.interval Show confidence interval (default behavior is TRUE)
-#' @param show.N Show confidence interval (default behavior is TRUE)
-#' @param ... Tidyverse selection of rows
+#' @param show.mean Show mean (Bool)
+#' @param show.sd Show standard deviation ((Bool))
+#' @param show.se Show standard error (Bool)
+#' @param show.conf.interval Show confidence interval (Bool)
+#' @param show.N Show number of cases (Bool)
 #' @return R Markdown text
 #' @examples
 #' #2-way ANOVA Example
@@ -21,13 +24,15 @@
 #' #Cell Mean: Female, 2 Pints
 #' apa.desc(goggles, alcohol=="2 Pints", gender=="Female", dv = attractiveness)
 #' @export
-apa.desc <- function(..., dv = NULL, data = NULL, show.mean = TRUE, show.sd = TRUE, show.se = FALSE, show.conf.interval = NULL, show.N = NULL) {
+apa.desc <- function(..., dv = NULL, data = NULL, show.mean = NULL, show.sd = NULL, show.se = NULL, show.conf.interval = NULL, show.N = NULL, number.decimals = NULL) {
 
   local_options <- set_local_options(list(show.mean = show.mean,
                                           show.sd = show.sd,
                                           show.se = show.se,
                                           show.conf.interval = show.conf.interval,
-                                          show.N = show.N))
+                                          show.N = show.N,
+                                          number.decimals = number.decimals))
+
   if (!is.null(get_apa_data())) {
     message("Using apa.data data frame.")
     assign("data", get("apa.data", envir = get_apa_data()))
@@ -43,23 +48,32 @@ apa.desc <- function(..., dv = NULL, data = NULL, show.mean = TRUE, show.sd = TR
   desc <- get_desc(group_data)
 
   # make markdown output
+  number_decimals <- local_options$number.decimals
   output_txt <- ""
 
   if (local_options$show.mean == TRUE) {
-    output_txt <- add_to_output(output_txt, sprintf("*M* = %1.2f", desc$group_m) )
+    new_str <- sprintf("*M* = %%1.%df",number_decimals)
+    output_txt <- add_to_output(output_txt, sprintf(new_str, desc$group_m) )
+    #output_txt <- add_to_output(output_txt, sprintf("*M* = %1.2f", desc$group_m) )
+  }
+
+  if (local_options$show.conf.interval == TRUE) {
+    #new_str <- sprintf("*M* = %%1.%df",number_decimals)
+    new_str <- sprintf("95%%%% CI[%%1.%df, %%1.%df]",number_decimals, number_decimals)
+    output_txt <- add_to_output(output_txt, sprintf(new_str, desc$LL, desc$UL) )
+    #output_txt <- add_to_output(output_txt, sprintf("95%% CI[%1.2f, %1.2f]", desc$LL, desc$UL) )
   }
 
   if (local_options$show.sd == TRUE) {
-    output_txt <- add_to_output(output_txt, sprintf("*SD* = %1.2f", desc$group_sd) )
+    new_str <- sprintf("*SD* = %%1.%df",number_decimals)
+    output_txt <- add_to_output(output_txt, sprintf(new_str, desc$group_sd) )
+    #output_txt <- add_to_output(output_txt, sprintf("*SD* = %1.2f", desc$group_sd) )
   }
 
   if (local_options$show.se == TRUE) {
-    output_txt <- add_to_output(output_txt, sprintf("*SE* = %1.2f", desc$group_se) )
-  }
-
-  # make markdown output
-  if (local_options$show.conf.interval == TRUE) {
-    output_txt <- add_to_output(output_txt, sprintf("95%% CI[%1.2f, %1.2f]", desc$LL, desc$UL) )
+    new_str <- sprintf("*SE* = %%1.%df",number_decimals)
+    output_txt <- add_to_output(output_txt, sprintf(new_str, desc$group_se) )
+    #output_txt <- add_to_output(output_txt, sprintf("*SE* = %1.2f", desc$group_se) )
   }
 
   if (local_options$show.N == TRUE) {
@@ -74,14 +88,17 @@ apa.desc <- function(..., dv = NULL, data = NULL, show.mean = TRUE, show.sd = TR
 
 
 
-get_desc <- function(group_data) {
+get_desc <- function(group_data, conf.level = .95) {
 
   N <- length(group_data)
   group_m <- mean(group_data, na.rm = TRUE)
   group_sd <- stats::sd(group_data, na.rm = TRUE)
   group_se <- group_sd / sqrt(N)
   group_df <- N - 1
-  ci_t <- stats::qt(.975, df = group_df) # 95% CI
+
+  qtlookup <- conf.level + (1-conf.level)/2
+  ci_t <- stats::qt(qtlookup, df = group_df)
+
   LL <- group_m - ci_t * group_se
   UL <- group_m + ci_t * group_se
 
